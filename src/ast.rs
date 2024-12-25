52c64b94b70a5fea551b0447e95d4e887a54dfc6
Freya Arbjerg <freya@arbjerg.dev>
2024-12-25 21:54:21 +0100

    Add if/else if/else test

--- src/ast.rs
+++ src/ast.rs
@@ -13,14 +13,14 @@ pub enum BiOperatorType {
 pub enum AstElement {
     Let { name: String, assignment: Box<AstElement> },
     Assignment { statement: Box<AstElement> },
-    If { condition: Box<AstElement>, then: Vec<AstElement> },
+    If { condition: Box<AstElement>, then: Vec<AstElement>, other_blocks: Vec<AstElement> },
     ElseIf { condition: Box<AstElement>, then: Vec<AstElement> },
     Else { then: Vec<AstElement> },
     BiOperator { bi_operator_type: BiOperatorType, left: Box<AstElement>, right: Box<AstElement> },
     NumberLiteral { value: String },
     StringLiteral { value: String },
     FunctionCall { name: String, arguments: Vec<AstElement> },
-    Symbol { name: String }
+    Symbol { name: String },
 }
 
 pub fn parse(tokens: Vec<Vec<Token>>) -> Result<Vec<AstElement>, String> {
@@ -31,8 +31,9 @@ pub fn parse(tokens: Vec<Vec<Token>>) -> Result<Vec<AstElement>, String> {
 
 #[cfg(test)]
 mod test {
+    use std::vec;
     use crate::ast::{parse, AstElement, BiOperatorType};
-    use crate::ast::AstElement::Let;
+    use crate::ast::AstElement::{If, Let, StringLiteral};
     use crate::lexer;
 
     #[test]
@@ -54,7 +55,46 @@ mod test {
                 )
             },
         ];
-        
+
+        assert_eq!(parse(lexed), Ok(expected));
+    }
+
+    #[test]
+    fn test_if_else_then() {
+        let source = include_str!("test/ast_if_elseif_else.enby").to_string();
+        let lexed = lexer::lex_from_string(source).unwrap();
+
+        let expected = vec![
+            If {
+                condition: Box::new(AstElement::Symbol { name: "foo".to_string() }),
+                then: vec![AstElement::FunctionCall {
+                    name: "println".to_string(),
+                    arguments: vec![
+                        StringLiteral { value: "foo".to_string() }
+                    ]
+                }],
+                other_blocks: vec![
+                    AstElement::ElseIf {
+                        condition: Box::new(AstElement::Symbol { name: "bar".to_string() }),
+                        then: vec![AstElement::FunctionCall {
+                            name: "println".to_string(),
+                            arguments: vec![
+                                StringLiteral { value: "bar".to_string() }
+                            ]
+                        }]
+                    },
+                    AstElement::Else {
+                        then: vec![AstElement::FunctionCall {
+                            name: "println".to_string(),
+                            arguments: vec![
+                                StringLiteral { value: "baz".to_string() }
+                            ]
+                        }]
+                    }
+                ],
+            }
+        ];
+
         assert_eq!(parse(lexed), Ok(expected));
     }
 }
