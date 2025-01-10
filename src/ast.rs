2316d158f7c6c7f4c6149db8e8d2d7f960e68436
Freya Arbjerg <freya@arbjerg.dev>
2025-01-10 20:48:34 +0100

    Move statement code into its own module

--- src/ast.rs
+++ src/ast.rs
@@ -1,4 +1,5 @@
 pub mod operator;
+mod statement;
 
 use crate::ast::AstElement::*;
 use crate::lexer::{Token, TokenType};
@@ -60,7 +61,7 @@ fn parse_let(remaining_tokens: &mut Vec<Token>) -> Result<AstElement, String> {
         return Err("Expected equals sign".to_string())
     }
     
-    let statement = parse_statement(tokens)?;
+    let statement = statement::parse(tokens)?;
     Ok(Let { name: symbol_name, statement: Box::new(statement) })
 }
 
@@ -69,37 +70,6 @@ fn parse_reassignment() -> Result<AstElement, String> {
 }
 
 fn parse_if_blocks() -> Result<AstElement, String> {
-    todo!()
-}
-
-fn parse_statement(tokens: Vec<Token>) -> Result<AstElement, String> {
-    let mut tokens_with_operators: Vec<(Token, Option<OperatorType>)> = Vec::new();
-    
-    for token in tokens {
-        let tuple = match token.token_type { 
-            TokenType::Special => { 
-                let operator_opt = operator::parse(token.contents.as_str());
-                (token, operator_opt)
-            },
-            TokenType::Symbol | TokenType::Number => (token, None),
-            _ => Err(format!("Unexpected {} when parsing statement", token.contents).to_string())?
-        };
-        tokens_with_operators.push(tuple);
-    }
-
-    parse_statement_recursive(tokens_with_operators)
-}
-
-fn parse_statement_recursive(mut tokens: Vec<(Token, Option<OperatorType>)>) -> Result<AstElement, String> {
-    if tokens.len() == 1 {
-        let (last_token, _) = tokens.pop().unwrap();
-        return Ok(match last_token.token_type { 
-            TokenType::Number => { NumberLiteral { value: last_token.contents } },
-            TokenType::Symbol => { Symbol { name: last_token.contents } }
-            _ => return Err(format!("Unexpected token '{}' in statement", last_token.contents))
-        })
-    }
-    
     todo!()
 }
 
