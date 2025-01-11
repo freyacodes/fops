5c4175dc8560ffc2e92805387fc39d0015694458
Freya Arbjerg <freya@arbjerg.dev>
2025-01-11 14:08:41 +0100

    Implement unary interpretation

--- src/ast/operator.rs
+++ src/ast/operator.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 #[derive(PartialEq, Debug)]
 pub enum OperatorType {
     Equality,
@@ -11,14 +13,20 @@ pub enum OperatorType {
 pub(super) fn parse(str: &str) -> Option<OperatorType> {
     Some(match str {
         "==" => OperatorType::Equality,
-        
+
         "*" => OperatorType::Multiplication,
         "/" => OperatorType::Division,
         "%" => OperatorType::Modulus,
-        
+
         "+" => OperatorType::Addition,
         "-" => OperatorType::Subtraction,
-        
+
         _ => None?
     })
+}
+
+impl fmt::Display for OperatorType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
 }
--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,8 +1,13 @@
+use std::cmp::PartialEq;
+use std::fmt::{format, Display, Formatter};
 use crate::ast::AstElement;
+use crate::ast::operator::OperatorType;
 
-enum RuntimeValue {
+#[derive(Debug)]
+pub enum RuntimeValue {
     String(String),
-    Number(i64),
+    Integer(i32),
+    Boolean(bool)
 }
 
 fn evaluate_expression(element: &AstElement) -> Result<RuntimeValue, String> {
@@ -13,10 +18,27 @@ fn evaluate_expression(element: &AstElement) -> Result<RuntimeValue, String> {
         AstElement::ElseIf { .. } => panic!("Not intended to be used here"),
         AstElement::Else { .. } => panic!("Not intended to be used here"),
         AstElement::BiOperator { operator, left, right } => todo!(),
-        AstElement::UnaryOperator { operator, operand } => todo!(),
-        AstElement::NumberLiteral { value } => RuntimeValue::Number(value.parse().unwrap()),
+        AstElement::UnaryOperator { operator, operand } => {
+            if operator != &OperatorType::Subtraction { panic!("Unexpected operator {}", operator) }
+            let operand_value = evaluate_expression(operand)?;
+            match operand_value { 
+                RuntimeValue::Integer(i) => RuntimeValue::Integer(-i),
+                _ => return error_expected_integer(&operand_value)
+            }
+        },
+        AstElement::NumberLiteral { value } => {
+            return if let Ok(number) = value.parse() {
+                Ok(RuntimeValue::Integer(number))
+            } else {
+                Err(format!("Can't parse integer: {}", value))
+            }
+        },
         AstElement::StringLiteral { value } => RuntimeValue::String(value.clone()),
-        AstElement::FunctionCall { name, arguments } => todo!(),
+        AstElement::FunctionCall { name, arguments } => todo!("Functions are not a thing yet"),
         AstElement::Symbol { name } => todo!("Not implemented until variables are added")
     })
+}
+
+fn error_expected_integer(value: &RuntimeValue) -> Result<RuntimeValue, String> {
+    Err(format!("Expected an integer, got {:?}", value))
 }
