742849d5b77e2364cf13ba5c834e784a3c4928b9
Freya Arbjerg <freya@arbjerg.dev>
2025-01-17 23:57:46 +0100

    Fix bad test_expression_statement, add test_assignment_statement

--- src/ast/statement.rs
+++ src/ast/statement.rs
@@ -5,6 +5,14 @@ use crate::lexer::Token;
 use std::collections::VecDeque;
 
 pub(super) fn statement(tokens: &mut VecDeque<Token>) -> Result<AstStatement, String> {
+    assignment_statement(tokens)
+}
+
+fn assignment_statement(tokens: &mut VecDeque<Token>) -> Result<AstStatement, String> {
+    reassignment_statement(tokens)
+}
+
+fn reassignment_statement(tokens: &mut VecDeque<Token>) -> Result<AstStatement, String> {
     expression_statement(tokens)
 }
 
@@ -16,20 +24,36 @@ fn expression_statement(tokens: &mut VecDeque<Token>) -> Result<AstStatement, St
 
 #[cfg(test)]
 mod test {
-    use crate::ast::AstExpression::{FunctionCall, StringLiteral};
-    use crate::ast::expression::expression;
+    use crate::ast::statement::statement;
+    use crate::ast::AstExpression::{FunctionCall, NumberLiteral, StringLiteral};
+    use crate::ast::AstStatement;
     use crate::lexer;
+
+    #[test]
+    fn test_assignment_statement() {
+        let mut lexed = lexer::lex_from_string("let four = 4;".to_string()).unwrap();
+        let statement = statement(&mut lexed).expect("Expected to return Ok");
+
+        assert_eq!(statement, AstStatement::Assignment {
+            name: "four".to_string(),
+            expression: Box::new(NumberLiteral {
+                value: "4".to_string(),
+            })
+        });
+    }
 
     #[test]
     fn test_expression_statement() {
         let mut lexed = lexer::lex_from_string("println(\"Hello, world!\");".to_string()).unwrap();
-
-        let expression = expression(&mut lexed).expect("Expected to return Ok");
-        assert_eq!(expression, FunctionCall {
-            name: "println".to_string(),
-            arguments: vec![
-                StringLiteral { value: "Hello, world!".to_string() }
-            ]
+        let statement = statement(&mut lexed).expect("Expected to return Ok");
+        
+        assert_eq!(statement, AstStatement::Expression {
+            expression: Box::new(FunctionCall {
+                name: "println".to_string(),
+                arguments: vec![
+                    StringLiteral { value: "Hello, world!".to_string() }
+                ]
+            })
         });
     }
 }
