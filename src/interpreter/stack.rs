1d2c54cb8ee665d298ff4cd1957f376ea75dc5c3
Freya Arbjerg <freya@arbjerg.dev>
2025-01-28 23:26:40 +0100

    Reimplement environment as a stack

--- src/interpreter/stack.rs
+++ src/interpreter/stack.rs
@@ -1,38 +1,68 @@
 use crate::interpreter::value::RuntimeValue;
 use std::collections::hash_map::Entry;
-use std::collections::HashMap;
+use std::collections::{HashMap, VecDeque};
+use std::option;
 
 pub struct Stack {
     //parent: Option<Box<stack>>,
-    values: HashMap<String, RuntimeValue>
+    frames: VecDeque<StackFrame>,
+}
+
+struct StackFrame {
+    values: HashMap<String, RuntimeValue>,
 }
 
 impl Stack {
     pub fn new() -> Self {
-        Stack {
-            values: HashMap::new()
-        }
+        let mut frames = VecDeque::new();
+        frames.push_back(StackFrame::new());
+        Stack { frames }
     }
 
     pub fn get(&self, key: &str) -> Option<&RuntimeValue> {
-        self.values.get(key)
+        for frame in self.frames.iter().rev() {
+            let value = frame.values.get(key);
+            if value.is_some() { return value; }
+        }
+        None
     }
 
     pub fn declare(&mut self, key: String, value: RuntimeValue) -> Result<(), String> {
-        let entry = self.values.entry(key);
+        let entry = self.frames.back_mut()
+            .expect("No frames on the stack")
+            .values.entry(key);
+
         match entry {
-            Entry::Occupied(_) => { return Err(format!("Variable '{}' is already defined", entry.key())); },
+            Entry::Occupied(_) => { return Err(format!("Variable '{}' is already defined", entry.key())); }
             Entry::Vacant(_) => { entry.insert_entry(value); }
         }
         Ok(())
     }
 
     pub fn reassign(&mut self, key: &String, value: RuntimeValue) -> Result<(), String> {
-        let option = self.values.get_mut(key);
-        match option {
-            Some(v) => { *v = value },
-            None => { return Err(format!("Variable '{}' is not found", key)); }
+        for frame in self.frames.iter_mut().rev() {
+            if let Some(value_ref) = frame.values.get_mut(key) {
+                *value_ref = value;
+                return Ok(())
+            }
         }
-        Ok(())
+
+        Err(format!("Variable '{}' is not defined", key))
+    }
+
+    pub fn push_frame(&mut self) {
+        self.frames.push_back(StackFrame::new());
+    }
+
+    pub fn pop_frame(&mut self) {
+        self.frames.pop_front().expect("No frames on the stack");
+    }
+}
+
+impl StackFrame {
+    fn new() -> Self {
+        StackFrame {
+            values: HashMap::new()
+        }
     }
 }
