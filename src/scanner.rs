5af2059a964a12612f75d47194f6caa08dbb37fd
Freya Arbjerg <freya@arbjerg.dev>
2025-02-27 18:09:32 +0100

    Implement scanner for 1 or 2 length tokens
    Also implement whitespace handling

--- src/scanner.rs
+++ src/scanner.rs
@@ -19,13 +19,14 @@ pub struct Token<'a> {
 pub enum ScannnerResult<'a> {
     Ok(Token<'a>),
     Err(String),
-    EOF
+    EOF,
 }
 
 #[derive(Debug, PartialEq)]
+#[rustfmt::skip]
 pub enum TokenType {
     // Single character tokens
-    TokenLeftParen, TokenRightParen, 
+    TokenLeftParen, TokenRightParen,
     TokenLeftBrace, TokenRightBrace,
     TokenComma, TokenDot, TokenMinus, TokenPlus,
     TokenSemicolon, TokenSlash, TokenAsterisk,
@@ -33,9 +34,9 @@ pub enum TokenType {
     // One or two character tokens
     TokenBang, TokenBangEqual,
     TokenEqual, TokenEqualEqual,
-    TokenGreater, TokenGreaterEqual,
     TokenLess, TokenLessEqual,
-    TokenLogicalAnd, TokenLogicalOr,
+    TokenGreater, TokenGreaterEqual,
+    TokenAmpAmp, TokenPipePipe,
 
     // Literals
     TokenIdentifier, TokenString, TokenNumber,
@@ -46,18 +47,38 @@ pub enum TokenType {
 }
 
 impl<'a> Scanner {
-    pub fn new(source: String) -> Scanner { Self {
-        source,
-        current_token_start: 0,
-        current_token_end: 0,
-        line: 1
-    } }
+    pub fn new(source: String) -> Scanner {
+        Self {
+            source,
+            current_token_start: 0,
+            current_token_end: 0,
+            line: 1,
+        }
+    }
 
     pub fn next(&'a mut self) -> ScannnerResult<'a> {
-        if self.source.len() == self.current_token_end {
-            return EOF
+        // Skip whitespace
+        loop {
+            if self.is_at_end() {
+                return EOF;
+            }
+            let c = self.source.as_bytes()[self.current_token_end] as char;
+            match c {
+                ' ' | '\r' | '\t' => {
+                    self.current_token_end += 1;
+                }
+                '\n' => {
+                    self.line += 1;
+                    self.current_token_end += 1;
+                }
+                _ => {
+                    break;
+                }
+            }
         }
-
+        
+        self.current_token_start = self.current_token_end;
+        
         let c = self.advance();
         match c {
             '(' => return self.make_token(TokenLeftParen),
@@ -71,6 +92,48 @@ impl<'a> Scanner {
             '+' => return self.make_token(TokenPlus),
             '/' => return self.make_token(TokenSlash),
             '*' => return self.make_token(TokenAsterisk),
+            '!' => {
+                return if self.match_next('=') {
+                    self.make_token(TokenBangEqual)
+                } else {
+                    self.make_token(TokenBang)
+                }
+            }
+            '=' => {
+                return if self.match_next('=') {
+                    self.make_token(TokenEqualEqual)
+                } else {
+                    self.make_token(TokenEqual)
+                }
+            }
+            '<' => {
+                return if self.match_next('=') {
+                    self.make_token(TokenLessEqual)
+                } else {
+                    self.make_token(TokenLess)
+                }
+            }
+            '>' => {
+                return if self.match_next('=') {
+                    self.make_token(TokenGreaterEqual)
+                } else {
+                    self.make_token(TokenGreater)
+                }
+            }
+            '&' => {
+                return if self.match_next('&') {
+                    self.make_token(TokenAmpAmp)
+                } else {
+                    ScannnerResult::Err("Unexpected lone &".to_string())
+                }
+            }
+            '|' => {
+                return if self.match_next('|') {
+                    self.make_token(TokenPipePipe)
+                } else {
+                    ScannnerResult::Err("Unexpected lone |".to_string())
+                }
+            }
             _ => {}
         };
 
@@ -80,6 +143,21 @@ impl<'a> Scanner {
     fn advance(&mut self) -> char {
         self.current_token_end += 1;
         self.source.as_bytes()[self.current_token_end - 1] as char
+    }
+
+    fn match_next(&mut self, expected: char) -> bool {
+        if self.is_at_end() {
+            return false;
+        }
+        if self.source.as_bytes()[self.current_token_end] as char != expected {
+            return false;
+        }
+        self.current_token_end += 1;
+        true
+    }
+
+    fn is_at_end(&self) -> bool {
+        self.source.len() == self.current_token_end
     }
 
     fn make_token(&'a mut self, token_type: TokenType) -> ScannnerResult<'a> {
@@ -97,17 +175,28 @@ mod tests {
 
     fn match_token(scanner: &mut Scanner, token_type: TokenType) {
         match scanner.next() {
-            ScannnerResult::Ok(token) => { assert_eq!(token_type, token.token_type) }
-            ScannnerResult::Err(string) => { panic!("Unexpected error: {}", string); }
-            EOF => panic!("Unexpected EOF")
+            ScannnerResult::Ok(token) => {
+                assert_eq!(token_type, token.token_type)
+            }
+            ScannnerResult::Err(string) => {
+                panic!("Unexpected error: {}", string);
+            }
+            EOF => panic!("Unexpected EOF"),
         }
     }
-    
+
+    #[test]
+    fn blank_input() {
+        let source = "  \t \n".to_string();
+        let mut scanner = Scanner::new(source);
+        assert_eq!(scanner.next(), EOF);
+    }
+
     #[test]
     fn single_character_tokens() {
         let source = "(){},.-+;/*".to_string();
         let mut scanner = Scanner::new(source);
-        
+
         match_token(&mut scanner, TokenLeftParen);
         match_token(&mut scanner, TokenRightParen);
         match_token(&mut scanner, TokenLeftBrace);
@@ -121,5 +210,22 @@ mod tests {
         match_token(&mut scanner, TokenAsterisk);
         assert_eq!(scanner.next(), EOF);
     }
-    
+
+    #[test]
+    fn one_or_two_character_tokens() {
+        let source = "! != = == < <= > >= && ||".to_string();
+        let mut scanner = Scanner::new(source);
+
+        match_token(&mut scanner, TokenBang);
+        match_token(&mut scanner, TokenBangEqual);
+        match_token(&mut scanner, TokenEqual);
+        match_token(&mut scanner, TokenEqualEqual);
+        match_token(&mut scanner, TokenLess);
+        match_token(&mut scanner, TokenLessEqual);
+        match_token(&mut scanner, TokenGreater);
+        match_token(&mut scanner, TokenGreaterEqual);
+        match_token(&mut scanner, TokenAmpAmp);
+        match_token(&mut scanner, TokenPipePipe);
+        assert_eq!(scanner.next(), EOF);
+    }
 }
