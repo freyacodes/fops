c41ac4eb608d556516e818ee06c95dc45c1de719
Freya Arbjerg <freya@arbjerg.dev>
2025-03-04 21:57:45 +0100

    Do most of the scaffolding for the Pratt parser

--- src/compiler.rs
+++ src/compiler.rs
@@ -1,13 +1,13 @@
 use crate::bytecode::chunk::Chunk;
 use crate::bytecode::codes::*;
+use crate::scanner::TokenType::*;
 use crate::scanner::{Scanner, Token, TokenType, PLACEHOLDER_TOKEN};
 
 pub(crate) fn compile(source: String) -> Result<Chunk, ()> {
     let mut chunk = Chunk::new();
     let mut parser = Parser::init(&source, &mut chunk);
-
     parser.advance();
-    
+
     match parser.had_error {
         true => Err(()),
         false => {
@@ -26,7 +26,30 @@ struct Parser<'a> {
     panic_mode: bool,
 }
 
-impl<'a, 'b> Parser<'a> {
+#[derive(Ord, PartialOrd, Eq, PartialEq)]
+enum Precedence {
+    PrecNone = 0,
+    PrecAssignment = 1,
+    PrecOr = 2,
+    PrecAnd = 3,
+    PrecEquality = 4,
+    PrecComparison = 5,
+    PrecTerm = 6,
+    PrecFactor = 7,
+    PrecUnary = 8,
+    PrecCall = 9,
+    PrecPrimary = 10,
+}
+
+struct ParseRule<'a> {
+    prefix: Option<ParseFn<'a>>,
+    infix: Option<ParseFn<'a>>,
+    precedence: Precedence,
+}
+
+type ParseFn<'a> = fn(&mut Parser<'a>) -> ();
+
+impl<'a> Parser<'a> {
     fn init(source: &'a str, chunk: &'a mut Chunk) -> Parser<'a> {
         Parser {
             scanner: Scanner::new(source),
@@ -36,6 +59,16 @@ impl<'a, 'b> Parser<'a> {
             had_error: false,
             panic_mode: false,
         }
+    }
+
+    fn expression(&mut self) {
+        self.parse_presedence(Precedence::PrecAssignment);
+    }
+
+    fn parse_presedence(&mut self, precedence: Precedence) {}
+
+    fn get_rule(&self, operator_type: TokenType) -> ParseRule<'a> {
+        todo!()
     }
 
     fn consume(&mut self, token: TokenType, message: &str) {
@@ -59,36 +92,98 @@ impl<'a, 'b> Parser<'a> {
         }
     }
 
-    fn emit_bytse(&mut self, byte1: u8, byte2: u8) {
+    fn emit_bytes(&mut self, byte1: u8, byte2: u8) {
         self.chunk.write_simple(byte1);
         self.chunk.write_simple(byte2);
     }
-    
+
     fn emit_byte(&mut self, byte: u8) {
         self.chunk.write_simple(byte);
     }
-    
+
     fn error_at_current(&mut self, message: &str) {
         self.error_at(&self.current.clone(), message);
     }
-    
+
     fn error(&mut self, message: &str) {
         self.error_at(&self.previous.clone(), message);
     }
-    
+
     fn error_at(&mut self, token: &Token, message: &str) {
-        if self.panic_mode { return; }
+        if self.panic_mode {
+            return;
+        }
         self.panic_mode = true;
-        
+
         eprint!("[Line {}] Error", token.line);
-        
+
         if token.token_type == TokenType::EOF {
             eprint!("at end of file");
         } else if token.token_type != TokenType::ScannerError {
             eprint!(" at {}", token.string);
         }
-        
+
         eprintln!("{}", message);
         self.had_error = true;
+    }
+}
+
+/// Prefix parsing
+impl<'a> Parser<'a> {
+    fn grouping(&mut self) {
+        self.expression();
+        self.consume(TokenRightParen, "Expected ')' after expression.");
+    }
+
+    fn number(&mut self) {
+        match self.previous.string.parse::<f32>() {
+            Ok(value) => self.chunk.write_constant_f32(value),
+            Err(_) => self.error("Failed to parse number."),
+        }
+    }
+
+    fn unary(&mut self) {
+        let operator_type = self.previous.token_type;
+        self.expression();
+
+        match operator_type {
+            TokenMinus => self.emit_byte(OP_NEGATE),
+            _ => unreachable!(),
+        }
+    }
+}
+
+/// Infix parsing
+impl<'a> Parser<'a> {
+    fn binary(&mut self) {
+        let operator_type = self.previous.token_type;
+        let rule = self.get_rule(operator_type);
+        self.parse_presedence(rule.precedence.next());
+
+        match operator_type {
+            TokenPlus => self.emit_byte(OP_ADD),
+            TokenMinus => self.emit_byte(OP_SUBTRACT),
+            TokenAsterisk => self.emit_byte(OP_MULTIPLY),
+            TokenSlash => self.emit_byte(OP_DIVIDE),
+            _ => unreachable!(),
+        }
+    }
+}
+
+impl Precedence {
+    fn next(&self) -> Precedence {
+        match self {
+            Precedence::PrecNone => Precedence::PrecAssignment,
+            Precedence::PrecAssignment => Precedence::PrecOr,
+            Precedence::PrecOr => Precedence::PrecAnd,
+            Precedence::PrecAnd => Precedence::PrecEquality,
+            Precedence::PrecEquality => Precedence::PrecComparison,
+            Precedence::PrecComparison => Precedence::PrecTerm,
+            Precedence::PrecTerm => Precedence::PrecFactor,
+            Precedence::PrecFactor => Precedence::PrecUnary,
+            Precedence::PrecUnary => Precedence::PrecCall,
+            Precedence::PrecCall => Precedence::PrecPrimary,
+            Precedence::PrecPrimary => Precedence::PrecNone,
+        }
     }
 }
