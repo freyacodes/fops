e71ba5fd59cc03c6c5d0cf2ae6016d27e82f37d8
Freya Arbjerg <freya@arbjerg.dev>
2025-03-29 18:54:45 +0100

    Reorganise proc-macro

---
+++ fops-macros/src/opcodes.rs
@@ -0,0 +1,75 @@
+use proc_macro::TokenStream;
+use quote::{quote, ToTokens, TokenStreamExt};
+use syn::{parse_macro_input, Token};
+use syn::parse::{Parse, ParseStream};
+use syn::punctuated::Punctuated;
+
+pub fn opcodes(item: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(item as OpcodesInput);
+
+    let module_ident = input.module_ident;
+    let entries = input.entries.iter();
+    let count = entries.len();
+    let instruction_names = input.entries.iter().map(|e| e.ident.to_string()).collect::<Vec<String>>();
+    let instruction_sizes = input.entries.iter().map(|e| e.length as u8).collect::<Vec<u8>>();
+
+    quote!(
+        pub mod #module_ident {
+            #(#entries)*
+
+            pub const INSTRUCTION_NAMES: [&str; #count] = [#(#instruction_names,)*];
+            pub const INSTRUCTION_LENGTH: [u8; #count] = [#(#instruction_sizes,)*];
+        }
+    ).into()
+}
+
+struct OpcodesInput {
+    module_ident: syn::Ident,
+    _colon: Token![:],
+    entries: Punctuated<OpcodeEntry, Token![,]>,
+}
+
+struct OpcodeEntry {
+    code: syn::LitInt,
+    ident: syn::Ident,
+    length: usize,
+}
+
+mod kw {
+    syn::custom_keyword!(len);
+}
+
+impl Parse for OpcodesInput {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        Ok(OpcodesInput {
+            module_ident: input.parse()?,
+            _colon: input.parse()?,
+            entries: input.parse_terminated(OpcodeEntry::parse, Token![,])?
+        })
+    }
+}
+
+impl Parse for OpcodeEntry {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let code = input.parse()?;
+        input.parse::<Token![=]>()?;
+        let ident = input.parse()?;
+        let size = if input.peek(kw::len) {
+            input.parse::<kw::len>()?;
+            input.parse::<syn::LitInt>()?.base10_parse()?
+        } else {
+            1
+        };
+        Ok(OpcodeEntry { code, ident, length: size })
+    }
+}
+
+impl ToTokens for OpcodeEntry {
+    fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
+        let name = &self.ident;
+        let code = &self.code;
+        tokens.append_all(quote! {
+            pub const #name: u8 = #code;
+        })
+    }
+}
--- src/vm.rs
+++ src/vm.rs
@@ -64,7 +64,7 @@ pub fn run(chunk: &Chunk) -> Result<Value, String> {
             codes::OP_NIL => stack.push(NIL),
             codes::OP_TRUE => stack.push(TRUE),
             codes::OP_FALSE => stack.push(FALSE),
-            
+
             codes::OP_ADD => binary_op!(+, "addition"),
             codes::OP_SUBTRACT => binary_op!(-, "subtraction"),
             codes::OP_DIVIDE => binary_op!(/, "divide"),
@@ -80,7 +80,7 @@ pub fn run(chunk: &Chunk) -> Result<Value, String> {
                     },
                 };
             }
-            
+
             codes::OP_NOT => {
                 let value = stack.pop().expect("Stack is empty");
                 match value {
@@ -88,7 +88,7 @@ pub fn run(chunk: &Chunk) -> Result<Value, String> {
                     _ => return runtime_error(pc, &chunk, format!("Attempt to negate {}", value.to_string()))
                 }
             },
-            
+
             codes::OP_RETURN => return Ok(stack.pop().expect("Stack is empty")),
             _ => panic!("Unexpected opcode: {:04x}", instruction),
         }
