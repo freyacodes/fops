65c223721052e127c89404f173baee299a03a270
Freya Arbjerg <freya@arbjerg.dev>
2025-05-12 18:16:51 +0200

    Add string comparison test

--- src/vm/tests/strings.rs
+++ src/vm/tests/strings.rs
@@ -5,3 +5,12 @@ use fops_macros::vm_test;
 fn hello_world() {
     vm_test!("Hello, world!" => "Hello, world!");
 }
+
+#[test]
+fn comparison() {
+    vm_test!("Hello, world!", "Hello, world!", OP_EQUALS => true);
+    vm_test!("Hello, world!", "Hello, world!", OP_NOT_EQUALS => false);
+    
+    vm_test!("One", "Two", OP_EQUALS => false);
+    vm_test!("One", "Two", OP_NOT_EQUALS => true);
+}
